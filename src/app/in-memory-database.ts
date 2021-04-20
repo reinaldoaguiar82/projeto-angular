a1117a97ad627813dc2291c2f43acc3f1df0b799
Reinaldo de Aguiar <reinaldo@rrsistemas.com>
2021-04-20 11:14:13 -0300

    serviço categoria

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,14 +1,15 @@
 
 import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Categoria } from "./pages/categorias/shared/categoria.module";
 
 export class InMemoryDatabase implements InMemoryDbService {
     createDb(){
-        const categorias = [
+        const categorias: Categoria[] = [
            { id : 1, name: "Lazer", description : "Pagamentos de Contas da Casa"},
            { id : 2, name: "Saúde", description : "Plano de Saúde e Remédios"},
            { id : 3, name: "Lazer", description : "Cinema, parques, praia, etc"},
            { id : 4, name: "Salário", description : "Recebimento de salário"},
-           { id : 5, name: "Freelas", description : "Trabalhos extras"},
+           { id : 5, name: "Freelas", description : "Trabalhos extras"}
         ];
 
         return categorias;
---
+++ src/app/pages/categorias/shared/categoria.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+import { Observable, throwError } from "rxjs";
+import { map, catchError, flatMap } from "rxjs/operators";
+
+import { Categoria } from "./categoria.module";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  private apiPath : string = "api/categoria"
+
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  getAll(): Observable<Categoria[]>{
+    return this._http.get(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategorias)
+    );
+  }
+
+  getById(id: number): Observable<Categoria> {
+    const url = `${this.apiPath}/${id}`;
+    return this._http.get(url).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategoria)
+    )
+  }
+
+  create(categoria: Categoria): Observable<Categoria>{
+    return this._http.post(this.apiPath, Categoria).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategoria)
+    )
+  }
+
+  update(categoria: Categoria): Observable<Categoria>{
+    const url = `${this.apiPath}/${categoria.id}`;
+    return this._http.put(url, categoria).pipe(
+      catchError(this.handleError),
+      map(() => categoria)
+    )
+  }
+
+  delete(id:number):Observable<any>{
+    const url = `${this.apiPath}/${id}`;
+    return this._http.delete(url).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    )
+  }
+
+  //PRIVATE METODOS
+
+  private jsonDataToCategoria(jsonData: any): Categoria{
+    return jsonData as Categoria;
+  }
+
+  private jsonDataToCategorias(jsonData: any[]): Categoria[]{
+    const categorias: Categoria[] = [];
+    jsonData.forEach(element => categorias.push(element as Categoria));
+    return categorias;
+  }
+
+  private handleError(error: any): Observable<any>{
+    console.log('Erro na requisição', error);
+    return throwError(error);
+  }
+}
